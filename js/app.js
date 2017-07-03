b91f8c591b8b95ae8b323f21b65d31df3fe63119
Guilherme Silva Guini <guil_guini@hotmail.com>
2017-07-03 18:49:44 -0300

    Label gráfico

--- js/app.js
+++ js/app.js
@@ -54,32 +54,6 @@
             return dataRetorno;
         } ,
 
-        init : function() {
-           view.init();
-        }
-    };
-
-    var view =  {
-
-        exibirMensagem : function(msg) {
-            alert(msg);
-        } ,
-
-		renderizarDados : function() {
-			$('.json').hide();
-			$('.content').show();
-			var workspace     = document.getElementById('workspace');
-			var linkWorkSpace = document.getElementById('link-work-space');
-            var lastActivity  = document.getElementById('last-activity');
-
-			workspace.innerHTML = data.json.name;
-			linkWorkSpace.href = data.json.url;
-            lastActivity.innerHTML = 'Data da última atividade: ' + controller.obterDataFormatada(data.json.dateLastActivity);
-
-            this.montarGraficoPizza();
-            this.montarGraficoLinha();
-		} ,
-
         montarGraficoLinha : function () {
             var chart = {};
 
@@ -117,7 +91,7 @@
             chart.id = 'myChartBar';
             chart.tipo = 'bar';
             chart.title = 'Funcionalidades / Correções';
-            chartjs.renderChart(chart);
+            view.renderChart(chart);
         } ,
 
         montarGraficoPizza : function() {
@@ -148,23 +122,34 @@
                         }]
                      }
 
-            chartjs.renderChart(chart);
+            view.renderChart(chart);
         } ,
 
-		init: function() {
-            $('.content').hide();
-
-			var btn = document.getElementById('btnGerarGraf');
-
-            if(!btn) {
-                console.log('Erro inesperado.');
-            } else {
-                btn.addEventListener('click', controller.armazenarJson);
-            }
-		}
+        init : function() {
+           view.init();
+        }
     };
 
-    var chartjs = {
+    var view =  {
+
+        exibirMensagem : function(msg) {
+            alert(msg);
+        } ,
+
+		renderizarDados : function() {
+			$('.json').hide();
+			$('.content').show();
+			var workspace     = document.getElementById('workspace');
+			var linkWorkSpace = document.getElementById('link-work-space');
+            var lastActivity  = document.getElementById('last-activity');
+
+			workspace.innerHTML = data.json.name;
+			linkWorkSpace.href = data.json.url;
+            lastActivity.innerHTML = 'Data da última atividade: ' + controller.obterDataFormatada(data.json.dateLastActivity);
+
+            controller.montarGraficoPizza();
+            controller.montarGraficoLinha();
+		} ,
 
         renderChart : function(chart) {
             var ctx = document.getElementById(chart.id).getContext('2d');
@@ -185,7 +170,55 @@
                         }
                       }
             });
-        }
+        } ,
+
+		init: function() {
+            $('.content').hide();
+
+			var btn = document.getElementById('btnGerarGraf');
+
+            if(!btn) {
+                console.log('Erro inesperado.');
+            } else {
+                btn.addEventListener('click', controller.armazenarJson);
+            }
+
+
+        // Define a plugin to provide data labels
+        Chart.plugins.register({
+            afterDatasetsDraw: function(chart, easing) {
+                // To only draw at the end of animation, check for easing === 1
+                var ctx = chart.ctx;
+
+                chart.data.datasets.forEach(function (dataset, i) {
+                    var meta = chart.getDatasetMeta(i);
+                    if (!meta.hidden) {
+                        meta.data.forEach(function(element, index) {
+                            // Draw the text in black, with the specified font
+                            ctx.fillStyle = 'blue'; //'rgb(155, 155, 155)';
+
+                            var fontSize = 16;
+                            var fontStyle = 'normal';
+                            var fontFamily = 'Helvetica Neue';
+                            ctx.font = Chart.helpers.fontString(fontSize, fontStyle, fontFamily);
+
+                            // Just naively convert to string for now
+                            var dataString = dataset.data[index].toString();
+
+                            // Make sure alignment settings are correct
+                            ctx.textAlign = 'center';
+                            ctx.textBaseline = 'middle';
+
+                            var padding = 5;
+                            var position = element.tooltipPosition();
+                            ctx.fillText(dataString, position.x, position.y - (fontSize / 2) - padding);
+                        });
+                    }
+                });
+            }
+        });
+
+		}
     };
 
     controller.init();
