035dfa1e10752e55dd5762f9fb5e751f255bacf9
Guilherme <guil_guini@hotmail.com>
2017-07-10 17:57:06 -0300

    Alterações

--- js/app.js
+++ js/app.js
@@ -2,20 +2,18 @@
 
     'use strict'
 
-    const enumState = {
-        'incomplete' : 'incomplete',
-        'complete'   : 'complete'  ,
-        'emoji'      : ':warning:'
-    };
-
-    const enumColors = {
-        'pendente'  : 'black',
-        'incompleto': 'red',
-        'completo'  : 'green'
-    };
-
     var data = {
-        json: {}
+        json: {} ,
+        enumState: {
+           'incomplete' : 'incomplete',
+           'complete'   : 'complete'  ,
+           'emoji'      : ':warning:'
+         } ,
+         enumColors: {
+           'pendente'  : 'black',
+           'incompleto': 'red',
+           'completo'  : 'green'
+         } 
     };
 
     var controller = {
@@ -132,25 +130,38 @@
             return card;
         } ,
 
+        getSMSs : function() {
+            var sms = [];
+
+            data.json.actions.forEach(function(item) {
+                if(item.data.text != null ) {
+                    var smsSplit = item.data.text.split('-');
+                    sms.push({'sms': smsSplit[0], 'funcionalidade': smsSplit[1]});
+                }
+            });
+
+            return sms.sort(function(a, b){return (a.funcionalidade > b.funcionalidade) ? 1 : ((b.funcionalidade > a.funcionalidade) ? -1 : 0);});
+        } ,
+
         getCountCheckItems : function(checklist) {
-            var qtdeCompleto = 0, qtdePendente = 0, qtdeIncompleto = 0;
+            var qtdeCompleto = 0, qtdeAFazer = 0, qtdePendente = 0;
             var card = {};
 
             checklist.checkItems.forEach(function(item) {
-                if(item.state === enumState.incomplete) {
+                if(item.state === data.enumState.incomplete) {
 
-                    if(item.name.indexOf(enumState.emoji) !== -1)
-                        qtdeIncompleto++;
-                    else
+                    if(item.name.indexOf(data.enumState.emoji) !== -1)
                         qtdePendente++;
+                    else
+                        qtdeAFazer++;
                 } else {
                     qtdeCompleto++;
                 }
             });
 
             return { 'completo'  : qtdeCompleto,
-                     'pendente'  : qtdePendente,
-                     'incompleto': qtdeIncompleto,
+                     'aFazer'  : qtdeAFazer,
+                     'pendente': qtdePendente,
                      'card'      : this.getCard(checklist.idCard).name,
                      'idCard'    : checklist.idCard
                    };
@@ -174,7 +185,7 @@
                     var qtdeItem = this.getCountCheckItems(item);
                     objCheckGroupCard[indexItemExists].completo   += qtdeItem.completo;
                     objCheckGroupCard[indexItemExists].pendente   += qtdeItem.pendente;
-                    objCheckGroupCard[indexItemExists].incompleto += qtdeItem.incompleto;
+                    objCheckGroupCard[indexItemExists].aFazer += qtdeItem.aFazer;
                 }
                 else
                     objCheckGroupCard.push(controller.getCountCheckItems(item));
@@ -211,22 +222,6 @@
             document.getElementById('chart-details').innerHTML = '';
         } ,
 
-		renderizarDados : function() {
-			this.controlarExibicao();
-
-			var workspace     = document.getElementById('workspace');
-			var lastActivity = document.getElementById('last-activity');
-			var linkWorkSpace = document.getElementById('link-work-space');
-
-			workspace.innerHTML = data.json.name;
-			linkWorkSpace.href = data.json.url;
-            lastActivity.innerHTML = 'Data da última atividade: ' + controller.getFormatData(data.json.dateLastActivity);
-
-            this.montarGraficoPizza();
-            this.montarGraficoLinha();
-            this.renderizarGraficosFunc();
-		} ,
-
 		controlarExibicao : function() {
 			 if(localStorage.getItem("json") !== null) {
 				$('.json').hide();
@@ -241,11 +236,10 @@
 			 }
 		} ,
 
-        renderizarGraficosFunc : function() {
+        renderizarGraficosDetalhes : function() {
             var divChartDetails = document.getElementById('chart-details');
 
             var checkItemsList = controller.getCountCheckItemsCards();
-            var labels = ['A fazer', 'Pendentes', 'Concluídos'];
 
             checkItemsList.forEach(function(item) {
                 var chart = {};
@@ -258,74 +252,42 @@
 
                 divElem.appendChild(canvasElem);
                 divChartDetails.appendChild(divElem);
-
-               chart.data =
-               {
-                  labels: labels,
-                  datasets: [{
-                       label: labels[0],
-                       data: [item.pendente, 0, 0],
-                       backgroundColor: enumColors.pendente
-                     },
-                     {
-                       label: labels[1],
-                       data: [0, item.incompleto, 0],
-                       backgroundColor:  enumColors.incompleto
-                     },
-                     {
-                       label: labels[2],
-                       data: [0, 0, item.completo],
-                       backgroundColor: enumColors.completo
-                    }
-                  ]
-               };
-
-               chart.id = item.idCard;
-               chart.tipo = 'bar';
-               chart.title = item.card;
-               view.renderChart(chart);
+                
+               view.montarGrafico(
+                { 'aFazer': item.aFazer , 'pendente': item.pendente, 'concluido': item.completo },
+                { 'aFazer': 'A fazer', 'pendente': 'Pendentes', 'concluido': 'Concluídos' },
+                item.card, 'bar', item.idCard);
             });
         } ,
 
-        montarGraficoLinha : function () {
+        montarGrafico : function (objValores, labels, title, type, id) {
             var chart = {};
-
-            var qtdeCorrecao = controller.getCountComments();
-			var objQtdes = controller.getCountCheckListDoneAndDoing();
-			var qtdeMaxima = 0, resultadoDivisao = 0;
-
+          
             chart.data =
                {
-                  labels: ['Itens a fazer ou pendentes', 'Correções Pendentes', 'Itens concluídos'],
+                  labels: Object.values(labels),
                   datasets: [{
-                       label: 'Itens a fazer ou pendentes',
-                       data: [objQtdes.qtdeAFazer, 0, 0],
-                       backgroundColor: enumColors.pendente
+                       label: labels.aFazer,
+                       data: [objValores.aFazer, 0, 0],
+                       backgroundColor: data.enumColors.pendente
                      },
                      {
-                       label: 'Correções Pendentes',
-                       data: [0, qtdeCorrecao, 0],
-                       backgroundColor: enumColors.incompleto
+                       label: labels.pendente,
+                       data: [0, objValores.pendente, 0],
+                       backgroundColor: data.enumColors.incompleto
                     },
 
                      {
-                       label: 'Itens concluídos',
-                       data: [0, 0, objQtdes.qtdeFeita],
-                       backgroundColor: enumColors.completo
+                       label: labels.concluido,
+                       data: [0, 0, objValores.concluido],
+                       backgroundColor: data.enumColors.completo
                      }
                   ]
                };
 
-			resultadoDivisao = qtdeCorrecao / 10;
-			qtdeMaxima = (resultadoDivisao + 1) * 10;
-			if(qtdeMaxima - qtdeCorrecao < 5)
-				qtdeMaxima += 5;
-
-            chart.id = 'myChartBar';
-            chart.tipo = 'bar';
-            chart.title = 'Funcionalidades / Correções';
-		    chart.beginAtZero = true;
-			chart.max = qtdeMaxima;
+            chart.id = id;
+            chart.tipo = type;
+            chart.title = title;
             view.renderChart(chart);
         } ,
 
@@ -348,9 +310,18 @@
                             backgroundColor: chart.colors
                         }]
                      }
-			chart.beginAtZero= true;
-            chart.max= 100;
+			
             view.renderChart(chart);
+        } ,
+
+        monstarGraficoCorrecao : function() {
+            var qtdeCorrecao = controller.getCountComments();
+			var objQtdes = controller.getCountCheckListDoneAndDoing();
+
+            this.montarGrafico(
+                { 'aFazer': objQtdes.qtdeAFazer , 'pendente': qtdeCorrecao, 'concluido': objQtdes.qtdeFeita },
+                { 'aFazer': 'Itens a fazer ou pendentes', 'pendente': 'Correções Pendentes', 'concluido': 'Itens Concluídos' },
+                'Funcionalidades / Correções', 'bar', 'myChartBar');
         } ,
 
         renderChart : function(chart) {
@@ -373,24 +344,7 @@
             });
         } ,
 
-		init: function() {
-            this.controlarExibicao();
-
-			var btnNovo = document.getElementById('btnNovo');
-			var btn = document.getElementById('btnGerarGraf');
-
-            if(!btn) {
-                console.log('Erro inesperado.');
-            } else {
-                btn.addEventListener('click', controller.armazenarJson);
-            }
-
-			if(!btnNovo) {
-				console.log('Erro inesperado.');
-			} else {
-				btnNovo.addEventListener('click', controller.novaConsulta);
-			}
-
+        definirLabelsChart : function() {
              // Define a plugin to provide data labels
             Chart.plugins.register({
             afterDatasetsDraw: function(chart, easing) {
@@ -404,9 +358,9 @@
                             // Draw the text in black, with the specified font
                             ctx.fillStyle = 'blue'; //'rgb(155, 155, 155)';
 
-                            var fontSize = 16;
+                            var fontSize = 18;
                             var fontStyle = 'normal';
-                            var fontFamily = 'Helvetica Neue';
+                         var fontFamily = 'Helvetica Neue';
                             ctx.font = Chart.helpers.fontString(fontSize, fontStyle, fontFamily);
 
                             // Just naively convert to string for now
@@ -424,9 +378,88 @@
                         });
                     }
                 });
-            }
-        });
+              }
+            });
+        } ,
 
+        exibirDetalhesSMS : function() {
+            
+            var sms = controller.getSMSs();
+            
+            if(sms.length === 0)
+             return;
+
+            sms.forEach(function(item, pos) {
+               var newRow = $("<tr>");             
+               newRow.append('<td>' + (pos + 1) + '</td>');
+               newRow.append('<td>' + item.sms + '</td>');
+               newRow.append('<td>' + item.funcionalidade + '</td>');
+               newRow.append('<td></td>');
+               $('#smsTable').append(newRow);
+            });
+            
+            return false;
+        } ,
+
+        exibirSMSsSeparadas : function() {
+            var table = $('#smsTable').children('tbody').children('tr');
+            var strBuilder = '';
+            var valueTd = '';
+
+            $.each(table, function(key, value) {
+               valueTd = $(value).children('td')[1].innerHTML;                             
+               strBuilder += valueTd.substring(0, valueTd.length -1) + ';';
+            });
+
+            if(strBuilder.length > 0)
+               strBuilder = strBuilder.substring(0, strBuilder.length - 1);
+
+            view.exibirMensagem(strBuilder);
+        } ,
+
+        renderizarDados : function() {
+			this.controlarExibicao();
+
+			var workspace     = document.getElementById('workspace');
+			var lastActivity = document.getElementById('last-activity');
+			var linkWorkSpace = document.getElementById('link-work-space');
+
+			workspace.innerHTML = data.json.name;
+			linkWorkSpace.href = data.json.url;
+            lastActivity.innerHTML = 'Data da última atividade: ' + controller.getFormatData(data.json.dateLastActivity);
+
+            this.montarGraficoPizza();
+            this.monstarGraficoCorrecao();
+            this.renderizarGraficosDetalhes();
+            this.exibirDetalhesSMS();
+		} ,
+
+		init: function() {
+            this.controlarExibicao();
+
+			var btnNovo = document.getElementById('btnNovo');
+			var btn = document.getElementById('btnGerarGraf');
+            var btnSMSs = document.getElementById('btnSMSsSeparadas');
+
+            if(!btn) {
+                console.log('Erro inesperado.');
+            } else {
+                btn.addEventListener('click', controller.armazenarJson);
+            }
+
+			if(!btnNovo) {
+				console.log('Erro inesperado.');
+			} else {
+				btnNovo.addEventListener('click', controller.novaConsulta);
+			}
+
+            if(!btnSMSs) {
+                console.log('Erro inesperado.');                
+            } else {
+                btnSMSs.addEventListener('click', this.exibirSMSsSeparadas);
+            }
+
+            this.definirLabelsChart();
 		}
     };
 
